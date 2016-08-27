a217b5f714d70221b23ea871abf471ad47e755ef
Mher Kazandjian <mherkazandjian@gmail.com>
2016-08-27 12:31:10 +0200

    some cleanup

--- cooling_function/cooling_function.py
+++ cooling_function/cooling_function.py
@@ -4,6 +4,7 @@ calculate equilibrium population of species and the cooling function.
 """
 
 import pylab
+import numpy
 
 from astropy import units as u
 
@@ -54,52 +55,8 @@ if True:
 
     pylab.loglog(T_rng.value, lambda_vs_T_kin.si.value, '-o', label='cooling H2')
     pylab.loglog(T_rng.value, lambda_vs_T_kin_glover.si.value,
-               'r--', label='cooling H2 glover')
+                 'r--', label='cooling H2 glover')
     pylab.legend()
     pylab.show()
 
-pdb.set_trace()
-
-
-# creating the array for the radiation temperatures;
-# at the moment, same size of tkin but constant value @ 2.73
-# tcmb = zeros(T.size)
-# tcmb = 2*T
-#tcmb[:] = 2.*T[:]
-
-# cf = zeros(T.size)
-# fit = zeros(T.size)
-# for itemp in range(T.size):
-#     cf[itemp] = cooling_function
-#     fit[itemp] = fit_glover(T[itemp])
-#
-#     print cooling_function*1e13 # to have the output in erg cm-3 s-1
-
-
-# plt.plot(T, cf*1e13, 'x', T, fit, 'o')
-# plt.xscale('log')
-# plt.yscale('log')
-#plt.axes(xrange())
-#yaxes: 1e-29:1e-22
-#plt.legend('reaction rate [cm^{-3}s^{-1}]')
-# plt.show()
-
-## plotting the population densities
-
-# def plot_vj_populations(v, j, nvj):
-#     import pylab
-#     from mpl_toolkits.mplot3d import Axes3D
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-#     ax.plot(v, j, nvj, 'o')
-#     ax.axes.xaxis.set_ticks(unique(v))
-#     ax.axes.yaxis.set_ticks(unique(j)[::2])
-#     ax.set_xlabel('v')
-#     ax.set_ylabel('j')
-#     ax.set_zlabel('n(v,j)')
-#     pylab.show()
-
-# plot_vj_populations(vj_unique[0], vj_unique[1], log10(nvj.flatten()))
-
-
-print 'done reading!'
+print 'done'
