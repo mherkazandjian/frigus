ec98a7efd0066b96e853d20f8a3413bc907e33f6
Mher Kazandjian <mherkazandjian@gmail.com>
2017-02-24 18:20:49 +0100

    account for the bugfix of the glover fit

--- cooling_function/cooling_function.py
+++ cooling_function/cooling_function.py
@@ -21,11 +21,11 @@ import pdb
 from readers import DataLoader
 
 # density of the colliding species, in m^3
-nc_H = 1e2 * u.meter**-3
+nc_H = 1e6 * u.meter**-3
 
 # the kinetic temperature of the gas
 T_kin = 100.0 * u.Kelvin
-T_rad = 30.0 * u.Kelvin
+T_rad = 0.0 * u.Kelvin
 
 species_data = DataLoader().load('H2_lique')
 
@@ -33,6 +33,7 @@ pop_dens_equ = population_density_at_steady_state(species_data,
                                                   T_kin,
                                                   T_rad,
                                                   nc_H)
+
 cooling_rate = cooling_rate_at_steady_state(species_data,
                                             T_kin,
                                             T_rad,
@@ -51,7 +52,7 @@ if True:
 
     lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
     lambda_vs_T_kin_glover = u.Quantity([fit_glover(T_kin) for T_kin in
-                                         T_rng.value])
+                                         T_rng.value]) * nc_H
 
     pylab.loglog(T_rng.value, lambda_vs_T_kin.si.value, '-o', label='cooling H2')
     pylab.loglog(T_rng.value, lambda_vs_T_kin_glover.si.value,
