9b3fb1e14a11c02fb40f6a9c3a31877f0a8eb45f
Mher Kazandjian <mherkazandjian@gmail.com>
2017-02-24 22:14:38 +0100

    plot ratio of ortho para vs T_kin

--- cooling_function/cooling_function.py
+++ cooling_function/cooling_function.py
@@ -5,9 +5,9 @@ calculate equilibrium population of species and the cooling function.
 from __future__ import print_function
 import pylab
 import numpy
+import pdb
 
 from astropy import units as u
-
 import matplotlib.pyplot as plt
 
 import population
@@ -16,47 +16,81 @@ from population import (fit_glover,
                         population_density_at_steady_state)
 import utils
 
-import pdb
-
 from readers import DataLoader
-
-# density of the colliding species, in m^3
-nc_H = 1e6 * u.meter**-3
-
-# the kinetic temperature of the gas
-T_kin = 100.0 * u.Kelvin
-T_rad = 0.0 * u.Kelvin
 
 species_data = DataLoader().load('H2_lique')
 
-pop_dens_equ = population_density_at_steady_state(species_data,
-                                                  T_kin,
-                                                  T_rad,
-                                                  nc_H)
+# Calculate the population density and the cooling rate per particle
+# for one value of kinetic temperature and gas density of H
+if False:
 
-cooling_rate = cooling_rate_at_steady_state(species_data,
-                                            T_kin,
-                                            T_rad,
-                                            nc_H)
+    # density of the colliding species, in m^3
+    nc_H = 1e6 * u.meter ** -3
+    T_kin = 100.0 * u.Kelvin
+    T_rad = 0.0 * u.Kelvin
+
+    pop_dens_equ = population_density_at_steady_state(species_data,
+                                                      T_kin,
+                                                      T_rad,
+                                                      nc_H)
+
+    cooling_rate = cooling_rate_at_steady_state(species_data,
+                                                T_kin,
+                                                T_rad,
+                                                nc_H)
 
 # utils.load_ascii_matrix_data()
 
 if True:
-    print('this')
+
+    # density of the colliding species, in m^3
+    nc_H = 1e6 * u.meter ** -3
+    T_kin = 100.0 * u.Kelvin
+    T_rad = 0.0 * u.Kelvin
+
+    pylab.ion()
+    fig, axs = pylab.subplots(2)
+
     lambda_vs_T_kin = []
-    T_rng = species_data.raw_data.collision_rates_T_range
+    pop_dens_vs_T_kin = []
+
+    T_rng = species_data.raw_data.collision_rates_T_range[::5]
     for T_kin in T_rng:
+
         print(T_kin)
+
         lambda_vs_T_kin += [cooling_rate_at_steady_state(species_data,
-                                                         T_kin, T_rad, nc_H)]
+                                                         T_kin,
+                                                         T_rad,
+                                                         nc_H)]
+
+        pop_dens_vs_T_kin += [population_density_at_steady_state(species_data,
+                                                                 T_kin,
+                                                                 T_rad,
+                                                                 nc_H)]
 
     lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
     lambda_vs_T_kin_glover = u.Quantity([fit_glover(T_kin) for T_kin in
                                          T_rng.value]) * nc_H
 
-    pylab.loglog(T_rng.value, lambda_vs_T_kin.si.value, '-o', label='cooling H2')
-    pylab.loglog(T_rng.value, lambda_vs_T_kin_glover.si.value,
-                 'r--', label='cooling H2 glover')
+    axs[0].loglog(
+        T_rng.value, lambda_vs_T_kin.si.value,
+        '-o', label='cooling H2')
+    axs[0].loglog(
+        T_rng.value, lambda_vs_T_kin_glover.si.value,
+        'r--', label='cooling H2 glover')
+
+    pop_dens_vs_T_kin = numpy.array(pop_dens_vs_T_kin)[:, :, 0]
+    axs[1].plot(
+        T_rng.value,
+        lambda_vs_T_kin.si.value / lambda_vs_T_kin_glover.si.value,
+        '-', label='lambda / lambda_glover')
+
+    axs[1].plot(
+        T_rng.value,
+        pop_dens_vs_T_kin[:, 0] / pop_dens_vs_T_kin[:, 1],
+        '--', label='x_v_0_j_0 / x_v_0_j_1')
+
     pylab.legend()
     pylab.show()
 
