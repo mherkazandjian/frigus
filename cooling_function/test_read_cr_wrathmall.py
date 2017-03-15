9bbeba2d5cebb1e1ed0a3576be8f744947f679ae
Mher Kazandjian <mherkazandjian@gmail.com>
2017-03-15 20:39:30 +0100

    cleanup wrathmall test reader

--- cooling_function/test_read_cr_wrathmall.py
+++ cooling_function/test_read_cr_wrathmall.py
@@ -9,17 +9,3 @@ fname = os.path.expanduser('~/dynamics/cooling_function/fortran/Read/wrathmall/R
 x = read_collision_coefficients(fname)
 
 print('done')
-reader = read_cr_wrathmall.Reader('Read/wrathmall/H.ortho_H2')
-
-'''
-# definig the reader object
-reader = uga('/home/mher/tmp/foo/H2XBCphotodiss.cs')
-
-# showing info
-reader.info()
-
-# get the data of a certain transition
-en, cs = reader.data[4,1,2]
-'''
-
-print('done')
