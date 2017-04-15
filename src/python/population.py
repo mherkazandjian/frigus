2273da3c131bb2e23169b7f0458e65d61415c1dc
Mher Kazandjian <mherkazandjian@gmail.com>
2017-04-15 21:56:14 +0200

    implement lipovka cooling function fits

--- src/python/population.py
+++ src/python/population.py
@@ -572,3 +572,79 @@ def fit_glover(T):
         raise ValueError("""out of bound""")
 
     return retval * u.erg * u.s**-1 * u.cm**3
+
+
+def fit_lipovka(T, n_hd):
+    """
+    fit of the cooling rate of HD as a function of temperature (in K) in units
+    of erg x s^-1
+
+    .. todo:: add ref
+
+    :param T: .. todo:: add doc
+    :return: .. todo:: add doc
+    """
+    lt_kin = numpy.log10(T.value)
+    ln_hd = numpy.log10(n_hd.cgs.value)
+
+    if numpy.log10(100.0) <= lt_kin and lt_kin <= numpy.log10(20000.0):
+        retval = 10.**(- 42.57688 * lt_kin ** 0 * ln_hd ** 0
+                       + 0.92433 * lt_kin ** 0 * ln_hd ** 1
+                       + 0.54962 * lt_kin ** 0 * ln_hd ** 2
+                       - 0.07676 * lt_kin ** 0 * ln_hd ** 3
+                       + 0.00275 * lt_kin ** 0 * ln_hd ** 4
+
+                       + 21.93385 * lt_kin ** 1 * ln_hd ** 0
+                       + 0.77952 * lt_kin ** 1 * ln_hd ** 1
+                       - 1.06447 * lt_kin ** 1 * ln_hd ** 2
+                       + 0.11864 * lt_kin ** 1 * ln_hd ** 3
+                       - 0.00366 * lt_kin ** 1 * ln_hd ** 4
+
+                       - 10.19097 * lt_kin ** 2 * ln_hd ** 0
+                       - 0.54263 * lt_kin ** 2 * ln_hd ** 1
+                       + 0.62343 * lt_kin ** 2 * ln_hd ** 2
+                       - 0.07366 * lt_kin ** 2 * ln_hd ** 3
+                       + 0.002514 * lt_kin ** 2 * ln_hd ** 4
+
+                       + 2.19906 * lt_kin ** 3 * ln_hd ** 0
+                       + 0.11711 * lt_kin ** 3 * ln_hd ** 1
+                       - 0.13768 * lt_kin ** 3 * ln_hd ** 2
+                       + 0.01759 * lt_kin ** 3 * ln_hd ** 3
+                       - 0.000666317 * lt_kin ** 3 * ln_hd ** 4
+
+                       - 0.17334 * lt_kin ** 4 * ln_hd ** 0
+                       - 0.00835 * lt_kin ** 4 * ln_hd ** 1
+                       + 0.0106 * lt_kin ** 4 * ln_hd ** 2
+                       - 0.001482 * lt_kin ** 4 * ln_hd ** 3
+                       + 0.000061926 * lt_kin ** 4 * ln_hd ** 4)
+    else:
+        msg = """temperature value {} is out of bound""".format(T)
+        raise ValueError(msg)
+
+    return retval * u.erg * u.s**-1
+
+
+def fit_lipovka_low_density(T):
+    """
+    fit of the cooling rate of HD as a function of temperature (in K) in units
+    of erg x s^-1 for low temperature
+
+    .. todo:: add ref
+
+    :param T: .. todo:: add doc
+    :return: .. todo:: add doc
+    """
+
+    lt_kin = numpy.log10(T.value)
+
+    if numpy.log10(100.0) <= lt_kin and lt_kin <= numpy.log10(20000.0):
+        retval = 10.0**(- 42.45906
+                        + 21.90083 * lt_kin
+                        - 10.1954 * lt_kin**2
+                        + 2.19788 * lt_kin**3
+                        - 0.17286 * lt_kin**4)
+    else:
+        msg = """temperature value {} is out of bound""".format(T)
+        raise ValueError(msg)
+
+    return retval * u.erg * u.s**-1
