591c3575ca639ac1bed411102b16abdbe2dd1177
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-22 13:39:16 +0200

    add test for the glover fit

--- src/python/tests/test_fit_lipovka.py
+++ src/python/tests/test_fit_lipovka.py
@@ -2,10 +2,11 @@ from __future__ import print_function
 import numpy
 from numpy.testing import assert_allclose
 from astropy import units as u
-from population import fit_lipovka
+import py.test
+from population import fit_lipovka, fit_glover, fit_lipovka_low_density
 
 
-def test_that_lipovka_cooling_function_fit_is_computed_correctly():
+def test_that_lipovka_cooling_function_fit_is_computed_correctly_at_low_densities():
 
     n_hd = 1e6 * u.m**-3
     T = numpy.linspace(100.0, 2000.0, 100) * u.K
@@ -15,6 +16,22 @@ def test_that_lipovka_cooling_function_fit_is_computed_correctly():
     expected_values = [2.218094e-25, 7.523573e-24, 1.984604e-23]
     assert_allclose(
         cooling_function[[0, 50, -1]].cgs.value,
+        expected_values,
+        rtol=1e-6,
+        atol=0.0)
+
+
+def test_that_lipovka_cooling_function_fit_is_computed_correctly_at_high_denisties():
+
+    py.test.skip('not implemented')
+
+def test_that_glover_cooling_function_fit_is_computed_correctly():
+    expected_values = [3.6341638e-29, 1.5221534e-26, 4.8841725e-25]
+    assert_allclose(
+        u.Quantity([
+            fit_glover(100.0),
+            fit_glover(500.0),
+            fit_glover(1000.0)]).cgs.value,
         expected_values,
         rtol=1e-6,
         atol=0.0)
