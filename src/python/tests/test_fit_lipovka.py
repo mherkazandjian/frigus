7237ff4850de418c5c1353c1dc8237e9821f8b96
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-22 13:27:39 +0200

    add the integration test for the lipovka fit

---
+++ src/python/tests/test_fit_lipovka.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import numpy
+from numpy.testing import assert_allclose
+from astropy import units as u
+from population import fit_lipovka
+
+
+def test_that_lipovka_cooling_function_fit_is_computed_correctly():
+
+    n_hd = 1e6 * u.m**-3
+    T = numpy.linspace(100.0, 2000.0, 100) * u.K
+
+    cooling_function = fit_lipovka(T, n_hd)
+
+    expected_values = [2.218094e-25, 7.523573e-24, 1.984604e-23]
+    assert_allclose(
+        cooling_function[[0, 50, -1]].cgs.value,
+        expected_values,
+        rtol=1e-6,
+        atol=0.0)
