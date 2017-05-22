d56d4082a2b79a542383f246bfc78885fddd2cfe
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-22 13:53:19 +0200

    add placeholder tests for the cooling functions

---
+++ src/python/tests/test_cooling_functions.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import numpy
+from numpy.testing import assert_allclose
+from astropy import units as u
+import py.test
+
+def test_that_the_lipovka_cooling_function_is_computed_correctly():
+    pass
+
+def test_that_the_lique_cooling_function_is_computed_correctly():
+    pass
