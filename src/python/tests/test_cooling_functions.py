a5e26b2b102bc761d5be82b55ca49f4b5f7292cb
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-22 17:20:52 +0200

    add test for computing the H2 cooling function by using lique data

--- src/python/tests/test_cooling_functions.py
+++ src/python/tests/test_cooling_functions.py
@@ -42,4 +42,34 @@ def test_that_the_lipovka_cooling_function_is_computed_correctly():
     )
 
 def test_that_the_lique_cooling_function_is_computed_correctly():
-    pass
+
+    T_rad = 0.0 * u.K
+    nc_H = 1e6 * u.m ** -3
+
+    species_data = DataLoader().load('H2_lique')
+
+    T_array = u.Quantity([100.0, 500.0, 1000.0, 2000.0, 5000.0]) * u.K
+    cooling_rate_expected = u.Quantity(
+        [
+            1.091851e-28,  # T=100K
+            2.674649e-26,  # T=500K
+            6.645044e-25,  # T=1000K
+            8.351277e-24,  # T=2000K
+            1.016337e-22,  # T=5000K
+        ]
+    ) * u.erg / u.s
+
+    cooling_rate_for_T_array = u.Quantity(
+        [
+            cooling_rate_at_steady_state(
+                species_data,
+                T,
+                T_rad,
+                nc_H) for T in T_array
+        ]
+    )
+
+    assert_allclose(
+        cooling_rate_for_T_array.si.value, cooling_rate_expected.si.value,
+        rtol=1e-6, atol=0.0
+    )
