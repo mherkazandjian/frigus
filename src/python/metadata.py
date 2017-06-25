d9f767139af91ec561f5db83b5efb32fc6256766
Mher Kazandjian <mherkazandjian@gmail.com>
2017-06-25 22:35:27 +0200

    populate the metadata file with basic info

---
+++ src/python/metadata.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'frigus'
+project = "level population and cooling function computation."
+project_no_spaces = project.replace(' ', '')
+version = '0.0.1'
+description = '.. todo:: long description here.'
+authors = ['Carla Maria Coppola', 'Mher Kazandjian']
+authors_string = ', '.join(authors)
+emails = ['carla.coppola@uniba.it', 'mherkazandjian@gmail.com']
+license = 'GLP-3.0'
+copyright = '2017 ' + authors_string
+url = 'https://github.com/mherkazandjian/frigus'
