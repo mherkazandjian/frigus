b00b71e788dec79094dd7752f6eb6d89e11014d4
Mher Kazandjian <mherkazandjian@gmail.com>
2017-07-28 21:15:02 +0200

    add test and example for comparing numeric and analytic two level pop dens ratio

---
+++ src/python/examples/cooling_function_two_level.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+<keywords>
+frigus, cooling, funtion, two, level, analytic, test
+</keywords>
+<description>
+compute the cooling function of a test two level system and compare it to
+the analytical result
+</description>
+<seealso>
+</seealso>
+"""
+from __future__ import print_function
+import numpy
+from astropy import units as u
+import matplotlib.pyplot as plt
+
+from frigus.readers import DataLoader
+from frigus.population import (
+    population_density_at_steady_state,
+    population_density_ratio_analytic_no_radiation
+)
+
+#
+# load the two level species data
+#
+species_data = DataLoader().load('two_level_1')
+
+n_c = 1e6 * u.meter ** -3
+T_kin = 100000.0 * u.Kelvin
+T_rad = 0.0 * u.Kelvin
+
+#
+# for each value in the temperature range
+#  - compute the population density ration numerically
+#  - compute the population density analytically
+#
+# plot the comaprision
+#
+ratio_numeric_vs_T_kin = []
+ratio_analytic_vs_T_kin = []
+T_kin_range = numpy.logspace(1.0, 10.0, 50) * u.Kelvin
+for T_kin in T_kin_range:
+
+    pop_dens_eq_numeric = population_density_at_steady_state(
+        species_data,
+        T_kin,
+        T_rad,
+        n_c)
+    pop_dens_eq_ratio_numeric = pop_dens_eq_numeric[1] / pop_dens_eq_numeric[0]
+    ratio_numeric_vs_T_kin.append(pop_dens_eq_ratio_numeric)
+
+    pop_dens_eq_ratio_analytic = population_density_ratio_analytic_no_radiation(
+        species_data.energy_levels.data['g'],
+        species_data.energy_levels.data['E'],
+        species_data.K_dex_matrix_interpolator(T_kin)[1, 0],
+        species_data.A_matrix[1, 0],
+        n_c,
+        T_kin
+    )
+    ratio_analytic_vs_T_kin.append(pop_dens_eq_ratio_analytic)
+    print(T_kin)
+
+ratio_numeric_vs_T_kin = numpy.array(ratio_numeric_vs_T_kin).flatten()
+ratio_analytic_vs_T_kin = numpy.array(ratio_analytic_vs_T_kin).flatten()
+
+#
+# plot the population density computed numericall and analyticall and also
+# plot the relative difference between them
+#
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
+ax0.loglog(T_kin_range, ratio_numeric_vs_T_kin, 'r+')
+ax0.loglog(T_kin_range, ratio_analytic_vs_T_kin, 'k--')
+ax0.set_xlabel('T_kin')
+ax0.set_title('x_1 / x_0')
+
+relative_error = numpy.abs(
+    1.0 - numpy.array(ratio_numeric_vs_T_kin) / numpy.array(
+        ratio_analytic_vs_T_kin)
+)
+ax1.loglog(T_kin_range, relative_error)
+ax1.set_xlabel('T_kin')
+ax1.set_title('relative error')
+
+plt.show()
+
+print('done')
---
+++ src/python/tests/test_two_level_system.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import numpy
+from numpy.testing import assert_allclose
+
+from astropy import units as u
+
+from frigus.readers import DataLoader
+from frigus.population import (
+    population_density_at_steady_state,
+    population_density_ratio_analytic_no_radiation
+)
+
+
+def test_that_two_level_population_density_ratios_agree_with_analytic_solution():
+
+    #
+    # load the two level species data
+    #
+    species_data = DataLoader().load('two_level_1')
+
+    n_c = 1e6 * u.meter ** -3
+    T_rad = 0.0 * u.Kelvin
+
+    #
+    # for each value in the temperature range
+    #  - compute the population density ration numerically
+    #  - compute the population density analytically
+    #
+    # plot the comaprision
+    #
+    ratio_numeric_vs_T_kin = []
+    ratio_analytic_vs_T_kin = []
+    T_kin_range = numpy.logspace(1.0, 10.0, 50) * u.Kelvin
+    for T_kin in T_kin_range:
+
+        pop_dens_eq_numeric = population_density_at_steady_state(
+            species_data,
+            T_kin,
+            T_rad,
+            n_c)
+        pop_dens_eq_ratio_numeric = pop_dens_eq_numeric[1] / pop_dens_eq_numeric[0]
+        ratio_numeric_vs_T_kin.append(pop_dens_eq_ratio_numeric)
+
+        pop_dens_eq_ratio_analytic = population_density_ratio_analytic_no_radiation(
+            species_data.energy_levels.data['g'],
+            species_data.energy_levels.data['E'],
+            species_data.K_dex_matrix_interpolator(T_kin)[1, 0],
+            species_data.A_matrix[1, 0],
+            n_c,
+            T_kin
+        )
+        ratio_analytic_vs_T_kin.append(pop_dens_eq_ratio_analytic)
+        print(T_kin)
+
+    ratio_numeric_vs_T_kin = numpy.array(ratio_numeric_vs_T_kin).flatten()
+    ratio_analytic_vs_T_kin = numpy.array(ratio_analytic_vs_T_kin).flatten()
+
+    relative_error = numpy.abs(
+        1.0 - numpy.array(ratio_numeric_vs_T_kin) / numpy.array(
+            ratio_analytic_vs_T_kin)
+    )
+
+    assert relative_error.max() < 5.0e-16
+    assert relative_error.std() < 1.0e-16
