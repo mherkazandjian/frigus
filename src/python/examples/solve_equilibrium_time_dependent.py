ba871412a7209dd03c8a3496cb6f81636b5db070
Mher Kazandjian <mherkazandjian@gmail.com>
2017-07-29 00:51:53 +0200

    add example of time evoltion

--- src/python/examples/solve_equilibrium_time_dependent.py
+++ src/python/examples/solve_equilibrium_time_dependent.py
@@ -3,50 +3,102 @@
 calculate equilibrium population of species and the cooling function.
 """
 from __future__ import print_function
-import pylab
 import numpy
 
 from astropy import units as u
 import matplotlib.pyplot as plt
+from scipy.integrate import ode
 
-from population import population_density_at_steady_state
-import utils
+from frigus.population import (
+    population_density_at_steady_state,
+    compute_transition_rate_matrix
+)
+from frigus.readers import DataLoader
 
-from readers import DataLoader
+#
+# load the species data
+#
+species_data = DataLoader().load('HD_lipovka')
 
-species_data = DataLoader().load('H2_lique')
-# species_data = DataLoader().load('HD_lipovka')
-def ode_rhs(t, y, args):
-    """defining the function which will be the rhs of df/dt"""
-    return dot(full, y)
+# time range and timestep
+t_0 = 0.0
+t_f = 1e10
+dt = 1e6
 
-# evolving with respect to time using scipy integrator
-if True:
-    r = ode(ode_rhs, jac = None).set_integrator('vode',
-    # r = ode(ode_rhs, jac = None).set_integrator('dopri',
-                                                method='bdf',
-                                                #with_jacobian = False,
-                                                rtol = 1e-3)
-    r.set_initial_value(f0, t0).set_f_params(1.0)
+# the environment parameters
+nc_H = 1e6 * u.meter ** -3
+T_kin = 100.0 * u.Kelvin
+T_rad = 2.73 * u.Kelvin
 
-    i = 0
-    # while r.successful() and r.t < tf:
-    while r.t < tf:
-        r.integrate(r.t + dt)
-        t.append(r.t)
-        ft_0.append(r.y[lPlot[0]])
-        ft_1.append(r.y[lPlot[1]])
-        ft_2.append(r.y[lPlot[2]])
-        ft_3.append(r.y[lPlot[3]])
-        ft_4.append(r.y[lPlot[4]])
-        ft_5.append(r.y[lPlot[5]])
+# the rates matrix (that is fixed if the environemnt params above are fixed)
+# so it is computed once
+M_matrix = compute_transition_rate_matrix(species_data, T_kin, T_rad, nc_H)
 
-        if i % 100 == 0:
-            print 'i = %d t = %e' % (i, r.t), 1.0 - numpy.sum(r.y), 1.0 - r.y[0]/f[0]
-            print 'stepNum = %d t = %e, current dt = %e, 1 - sum(pop_dens) = %e' % (i, r.t, dt, 1.0 - numpy.sum(r.y))
 
-        i += 1
+def ode_rhs(_, y, __):
+    """
+    define the function that computes the rhs of dy/dt
+    """
+    return numpy.dot(M_matrix, y)
 
+
+# set the initial abundances for t = 0
+n_levels = len(species_data.energy_levels.data)
+initial_fractional_abundances = numpy.ones(n_levels) / n_levels
+y_0 = initial_fractional_abundances
+
+
+# define the solver
+solver = ode(ode_rhs, jac=None).set_integrator('vode',
+                                               method='bdf',
+                                               # with_jacobian = False,
+                                               rtol=1e-3)
+
+solver.set_initial_value(y_0, t_0).set_f_params(1.0)
+
+step_counter = 0
+t_all = []
+x_all = []
+while solver.t < t_f:
+    solver.integrate(solver.t + dt)
+    t_all.append(solver.t)
+    x_all.append(solver.y)
+
+    if step_counter % 100 == 0:
+        print('{} t={:e} 1-x.sum()={:e}'.format(
+            step_counter, solver.t, 1.0 - solver.y.sum()
+        ))
+    step_counter += 1
+
+
+t_all, x_all = numpy.array(t_all), numpy.vstack(x_all)
+
+#
+# plot the solution
+#
+fig, axs = plt.subplots()
+
+axs.loglog(t_all, x_all[:, 0], 'r')
+axs.loglog(t_all, x_all[:, 1], 'g')
+axs.loglog(t_all, x_all[:, 2], 'b')
+axs.loglog(t_all, x_all[:, 3], 'k')
+
+# get the equilibrium solution and plot them as crosses at t = t_f
+pop_dens_eq = population_density_at_steady_state(
+    species_data,
+    T_kin,
+    T_rad,
+    nc_H)
+
+axs.loglog(t_all[-1], pop_dens_eq[0], 'r+')
+axs.loglog(t_all[-1], pop_dens_eq[1], 'g+')
+axs.loglog(t_all[-1], pop_dens_eq[2], 'b+')
+axs.loglog(t_all[-1], pop_dens_eq[3], 'k+')
+
+plt.ion()
+plt.show()
+
+'''
 # solving using my implementation of the bulrische stoer integrator
 if False:
     rel_tol = 1e-3
@@ -86,5 +138,6 @@ if False:
                    1.0 - numpy.sum(solver.state.y))
 
         stepNum += 1
+'''
 
 print('done')
