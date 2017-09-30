1440d4a6fc0e01f239bde499c5f3eb8a85ec5d2e
Mher Kazandjian <mherkazandjian@gmail.com>
2017-09-30 23:58:03 +0200

    add commented section to remove low rates and relax tolrenences for two tests

--- src/python/tests/test_solve_equilibium.py
+++ src/python/tests/test_solve_equilibium.py
@@ -16,7 +16,7 @@ def test_equilibrium_solver_snaity_2x2():
     x = solve_equilibrium(M_matrix)
 
     expected_x_values = numpy.array([[0.5], [0.5]])
-    assert_allclose(x, expected_x_values, rtol=1e-16, atol=0.0)
+    assert_allclose(x, expected_x_values, rtol=1e-15, atol=0.0)
 
 
 def test_equilibrium_solver_snaity_3x3():
@@ -31,7 +31,7 @@ def test_equilibrium_solver_snaity_3x3():
     x = solve_equilibrium(M_matrix)
 
     expected_x_values = numpy.array([[11.0/24.0], [1.0/2.0], [1.0/24.0]])
-    assert_allclose(x, expected_x_values, rtol=1e-15, atol=0.0)
+    assert_allclose(x, expected_x_values, rtol=1e-14, atol=0.0)
 
 
 
