bfad21b5f735aacd2a38472afeb45d798802df23
Carla Maria Coppola <cpcr01ch@gmail.com>
2017-11-23 13:07:51 +0100

    introduced in the figure the dependence on multiple values for the density (lique and lipovka case); the lique one is functional to the multivariate fit (to be explored)

--- src/python/examples/cooling_function_lipovka.py
+++ src/python/examples/cooling_function_lipovka.py
@@ -4,6 +4,7 @@ Dalculate equilibrium population of species and the cooling function using
 the Lipovka data
 """
 from __future__ import print_function
+import numpy as np
 import matplotlib.pyplot as plt
 
 from astropy import units as u
@@ -15,6 +16,10 @@ from frigus.population import (fit_lipovka,
 from frigus.readers import DataLoader
 
 species_data = DataLoader().load('HD_lipovka')
+
+plt.ion()
+fig, axs = plt.subplots()
+
 
 # Calculate the population density and the cooling rate per particle
 # for one value of kinetic temperature and gas density of H
@@ -40,41 +45,43 @@ if False:
 if True:
 
     # density of the colliding species, in m^3
-    nc_H = 1e6 * u.meter ** -3
+    # nc_H = 1e6 * u.meter ** -3
+    nc_H_rng = [1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14]
     T_rad = 0.0 * u.Kelvin
 
-    lambda_vs_T_kin = []
-    pop_dens_vs_T_kin = []
-
+    #T_rng = np.logspace(2, 3.2, 10)
     T_rng = species_data.raw_data.collision_rates_T_range
-    for T_kin in T_rng:
+    for nc_H in nc_H_rng:
+        lambda_vs_T_kin = []
+        pop_dens_vs_T_kin = []
+        nc_H = nc_H * u.meter ** -3
+        for T_kin in T_rng:
+            print(T_kin, nc_H)
 
-        print(T_kin)
 
-        lambda_vs_T_kin += [
-            cooling_rate_at_steady_state(
-                species_data,
-                T_kin,
-                T_rad,
-                nc_H)
-        ]
+            lambda_vs_T_kin += [
+                cooling_rate_at_steady_state(
+                    species_data,
+                    T_kin,
+                    T_rad,
+                    nc_H)]
 
-    lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
+        lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
 
-    lambda_vs_T_kin_lipovka = fit_lipovka(T_rng, nc_H)
+        lambda_vs_T_kin_lipovka = fit_lipovka(T_rng, nc_H)
 
-    plt.ion()
-    fig, axs = plt.subplots()
+        axs.loglog(
+            T_rng.value, lambda_vs_T_kin.si.value,
+            '-x', color = 'black', label='')
 
-    axs.loglog(
-        T_rng.value, lambda_vs_T_kin.si.value,
-        '-o', label='cooling HD')
+        axs.loglog(
+            T_rng.value, lambda_vs_T_kin_lipovka.si.value,
+            'r--', color = 'black', label='')
 
-    axs.loglog(
-        T_rng.value, lambda_vs_T_kin_lipovka.si.value,
-        'r--', label='cooling HD lipovka')
+        axs.set_xlabel('T$_\mathrm{kin}$ [K]')
+        axs.set_ylabel('cooling function [erg s$^{-1}$]')
 
-    plt.legend()
-    plt.show()
+        plt.legend()
+        plt.show()
 
 print('done')
