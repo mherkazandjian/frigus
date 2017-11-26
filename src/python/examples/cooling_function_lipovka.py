e643b1abeded9acc25a98cf48d39e3a749371e45
Mher Kazandjian <mherkazandjian@gmail.com>
2017-11-26 21:04:03 +0100

    clean up cooling func lipovka example

--- src/python/examples/cooling_function_lipovka.py
+++ src/python/examples/cooling_function_lipovka.py
@@ -9,79 +9,57 @@ import matplotlib.pyplot as plt
 
 from astropy import units as u
 
-from frigus.population import (fit_lipovka,
-                               cooling_rate_at_steady_state,
-                               population_density_at_steady_state)
+from frigus.population import (
+    fit_lipovka,
+    cooling_rate_at_steady_state
+)
 
 from frigus.readers import DataLoader
 
 species_data = DataLoader().load('HD_lipovka')
 
-plt.ion()
+plt.ioff()
 fig, axs = plt.subplots()
 
+# density of the colliding species, in m^3
+# nc_H = 1e6 * u.meter ** -3
+nc_H_rng = [1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14]
+T_rad = 0.0 * u.Kelvin
 
-# Calculate the population density and the cooling rate per particle
-# for one value of kinetic temperature and gas density of H
-if False:
-
-    # density of the colliding species, in m^3
-    nc_H = 1e6 * u.meter ** -3
-    T_kin = 100.0 * u.Kelvin
-    T_rad = 0.0 * u.Kelvin
-
-    pop_dens_equ = population_density_at_steady_state(
-        species_data,
-        T_kin,
-        T_rad,
-        nc_H)
-
-    cooling_rate = cooling_rate_at_steady_state(
-        species_data,
-        T_kin,
-        T_rad,
-        nc_H)
-
-if True:
-
-    # density of the colliding species, in m^3
-    # nc_H = 1e6 * u.meter ** -3
-    nc_H_rng = [1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14]
-    T_rad = 0.0 * u.Kelvin
-
-    T_rng = np.logspace(2, 3.2, 10) * u.Kelvin
-    # T_rng = species_data.raw_data.collision_rates_T_range
-    for nc_H in nc_H_rng:
-        lambda_vs_T_kin = []
-        pop_dens_vs_T_kin = []
-        nc_H = nc_H * u.meter ** -3
-        for T_kin in T_rng:
-            print(T_kin, nc_H)
+T_rng = np.logspace(2, 3.2, 10) * u.Kelvin
+# T_rng = species_data.raw_data.collision_rates_T_range
+for nc_H in nc_H_rng:
+    lambda_vs_T_kin = []
+    pop_dens_vs_T_kin = []
+    nc_H = nc_H * u.meter ** -3
+    for T_kin in T_rng:
+        print(T_kin, nc_H)
 
 
-            lambda_vs_T_kin += [
-                cooling_rate_at_steady_state(
-                    species_data,
-                    T_kin,
-                    T_rad,
-                    nc_H)]
+        lambda_vs_T_kin += [
+            cooling_rate_at_steady_state(
+                species_data,
+                T_kin,
+                T_rad,
+                nc_H)]
 
-        lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
+    lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
 
-        lambda_vs_T_kin_lipovka = fit_lipovka(T_rng, nc_H)
+    lambda_vs_T_kin_lipovka = fit_lipovka(T_rng, nc_H)
 
-        axs.loglog(
-            T_rng.value, lambda_vs_T_kin.si.value,
-            '-x', color = 'black', label='')
+    axs.loglog(
+        T_rng.value, lambda_vs_T_kin.si.value,
+        '-x', color = 'black', label='')
 
-        axs.loglog(
-            T_rng.value, lambda_vs_T_kin_lipovka.si.value,
-            'r--', color = 'black', label='')
+    axs.loglog(
+        T_rng.value, lambda_vs_T_kin_lipovka.si.value,
+        'r--', color = 'black', label='')
 
-        axs.set_xlabel('T$_\mathrm{kin}$ [K]')
-        axs.set_ylabel('cooling function [erg s$^{-1}$]')
+    axs.set_xlabel('T$_\mathrm{kin}$ [K]')
+    axs.set_ylabel('cooling function [erg s$^{-1}$]')
 
-        plt.legend()
-        plt.show()
+    plt.legend()
+
+plt.show()
 
 print('done')
