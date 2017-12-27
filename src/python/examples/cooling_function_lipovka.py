d47004cd71fd7e0eb6b32f2feadea8aea733a3cd
Mher Kazandjian <mherkazandjian@gmail.com>
2017-12-27 15:55:13 +0100

    use different plot symbols for each value of the density

--- src/python/examples/cooling_function_lipovka.py
+++ src/python/examples/cooling_function_lipovka.py
@@ -18,8 +18,7 @@ from frigus.readers import DataLoader
 species_data = DataLoader().load('HD_lipovka')
 
 plt.ion()
-fig, axs = plt.subplots()
-
+fig, axs = plt.subplots(figsize=(8, 8))
 
 # Calculate the population density and the cooling rate per particle
 # for one value of kinetic temperature and gas density of H
@@ -45,43 +44,50 @@ if False:
 if True:
 
     # density of the colliding species, in m^3
-    # nc_H = 1e6 * u.meter ** -3
-    nc_H_rng = [1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14]
+    nc_H_rng = [
+                   1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14
+               ] * u.meter ** -3
     T_rad = 0.0 * u.Kelvin
 
     T_rng = np.logspace(2, 3.2, 10) * u.Kelvin
     # T_rng = species_data.raw_data.collision_rates_T_range
-    for nc_H in nc_H_rng:
+
+    plot_markers = [(2 + i/2, 1 + i % 2, 0) for i in range(16)]
+
+    for nc_index, nc_H in enumerate(nc_H_rng):
+
         lambda_vs_T_kin = []
         pop_dens_vs_T_kin = []
-        nc_H = nc_H * u.meter ** -3
+
         for T_kin in T_rng:
             print(T_kin, nc_H)
-
 
             lambda_vs_T_kin += [
                 cooling_rate_at_steady_state(
                     species_data,
                     T_kin,
                     T_rad,
-                    nc_H)]
+                    nc_H)
+            ]
 
         lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
 
         lambda_vs_T_kin_lipovka = fit_lipovka(T_rng, nc_H)
 
         axs.loglog(
-            T_rng.value, lambda_vs_T_kin.si.value,
-            '-x', color = 'black', label='')
+            T_rng.value, lambda_vs_T_kin.cgs.value,
+            '-x', color='black', marker=plot_markers[nc_index], label=nc_H
+        )
 
         axs.loglog(
-            T_rng.value, lambda_vs_T_kin_lipovka.si.value,
-            'r--', color = 'black', label='')
+            T_rng.value, lambda_vs_T_kin_lipovka.cgs.value,
+            'r--', color='black', label=''
+        )
 
         axs.set_xlabel('T$_\mathrm{kin}$ [K]')
         axs.set_ylabel('cooling function [erg s$^{-1}$]')
 
-        plt.legend()
-        plt.show()
+    plt.legend()
+    plt.show()
 
 print('done')
