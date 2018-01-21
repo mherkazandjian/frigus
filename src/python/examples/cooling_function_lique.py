fe51d1a54ef92c3da2fe69851b0f785ae2324315
Mher Kazandjian <mherkazandjian@gmail.com>
2018-01-21 23:48:16 +0100

    revert back the lique cooling function script to before attempting to fit it

--- src/python/examples/cooling_function_lique.py
+++ src/python/examples/cooling_function_lique.py
@@ -17,13 +17,6 @@ from frigus.readers import DataLoader
 
 species_data = DataLoader().load('H2_lique')
 
-x_fit = []
-y_fit = []
-lambda_fit = []
-
-plt.ion()
-fig, axs = plt.subplots(2)
-
 # Calculate the population density and the cooling rate per particle
 # for one value of kinetic temperature and gas density of H
 if False:
@@ -48,59 +41,54 @@ if False:
 if True:
 
     # density of the colliding species, in m^3
-    # nc_H = 1e6 * u.meter ** -3
-    nc_H_rng = [1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e24]
+    nc_H = 1e6 * u.meter ** -3
     T_rad = 0.0 * u.Kelvin
 
+    lambda_vs_T_kin = []
+    pop_dens_vs_T_kin = []
+
     T_rng = species_data.raw_data.collision_rates_T_range
-        for nc_H in nc_H_rng:
-            pop_dens_vs_T_kin = []
+    for T_kin in T_rng:
 
-            d = []
-            nc_H = nc_H * u.meter ** -3
-            for T_kin in T_rng:
-                print(T_kin, nc_H)
+        print(T_kin)
 
-            pop_dens_vs_T_kin += [population_density_at_steady_state(species_data,
-                                                   T_kin,
-                                                   T_rad,
-                                                   nc_H)]
+        pop_dens_vs_T_kin += [population_density_at_steady_state(species_data,
+                                                                 T_kin,
+                                                                 T_rad,
+                                                                 nc_H)]
 
-            lambda_grid += [cooling_rate_at_steady_state(species_data,
+        lambda_vs_T_kin += [cooling_rate_at_steady_state(species_data,
                                                          T_kin,
                                                          T_rad,
                                                          nc_H)]
 
-            x_fit.append(T_kin.value)
-            y_fit.append(nc_H.cgs.value)
 
+    lambda_vs_T_kin = u.Quantity(lambda_vs_T_kin)
+    lambda_vs_T_kin_glover = u.Quantity([fit_glover(T_kin) for T_kin in
+                                         T_rng.value]) * nc_H
 
-        lambda_grid = u.Quantity(lambda_grid)
-        lambda_fit.append(lambda_grid.value)
+    plt.ion()
+    fig, axs = plt.subplots(2)
 
-        lambda_vs_T_kin_glover = u.Quantity([fit_glover(T_kin) for T_kin in
-                                             T_rng.value]) * nc_H
+    axs[0].loglog(
+        T_rng.value, lambda_vs_T_kin.cgs.value,
+        '-o', label='cooling H2')
+    axs[0].loglog(
+        T_rng.value, lambda_vs_T_kin_glover.cgs.value,
+        'r--', label='cooling H2 glover')
 
+    pop_dens_vs_T_kin = numpy.array(pop_dens_vs_T_kin)[:, :, 0]
+    axs[1].plot(
+        T_rng.value,
+        lambda_vs_T_kin.cgs.value / lambda_vs_T_kin_glover.cgs.value,
+        '-', label='lambda / lambda_glover')
 
-        axs[0].loglog(
-            T_rng.value, lambda_grid.si.value,
-            '-o', label='cooling H2')
-        axs[0].loglog(
-            T_rng.value, lambda_vs_T_kin_glover.si.value,
-            'r--', label='cooling H2 glover')
+    axs[1].plot(
+        T_rng.value,
+        pop_dens_vs_T_kin[:, 0] / pop_dens_vs_T_kin[:, 1],
+        '--', label='x_v_0_j_0 / x_v_0_j_1')
 
-        pop_dens_vs_T_kin = numpy.array(pop_dens_vs_T_kin)[:, :, 0]
-        axs[1].plot(
-            T_rng.value,
-            lambda_grid.si.value / lambda_vs_T_kin_glover.si.value,
-            '-', label='lambda / lambda_glover')
-
-        axs[1].plot(
-            T_rng.value,
-            pop_dens_vs_T_kin[:, 0] / pop_dens_vs_T_kin[:, 1],
-            '--', label='x_v_0_j_0 / x_v_0_j_1')
-
-        plt.legend()
-        plt.show()
+    plt.legend()
+    plt.show()
 
 print('done')
