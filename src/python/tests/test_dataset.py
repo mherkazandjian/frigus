cc7e894610122add3ef655bfdab2e4931eab66b6
Mher Kazandjian <mherkazandjian@gmail.com>
2018-02-27 00:51:59 +0100

    add placeholder tests

---
+++ src/python/tests/test_dataset.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import pytest
+
+
+def test_that_reduce_einstein_coefficients_slow_works_correctly():
+    pytest.skip('not implemented')
+
+
+def test_that_reduce_einstein_coefficients_works_correctly():
+    pytest.skip('not implemented')
+
+
