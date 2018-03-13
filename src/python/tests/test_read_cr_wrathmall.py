23d2791a4b6e731458c4a50395f41bb7330d77e4
Mher Kazandjian <mherkazandjian@gmail.com>
2018-03-13 23:36:43 +0100

    remove test module that was added by mistake

--- src/python/tests/test_read_cr_wrathmall.py
+++
@@ -1,9 +0,0 @@
-from frigus.readers.read_cr_wrathmall import Reader
-
-import pytest
-
-pytestmark = pytest.mark.skipif(True, reason='developemnt script')
-
-
-def test_that_read_can_be_constructed():
-    raise NotImplementedError('not implemented')
