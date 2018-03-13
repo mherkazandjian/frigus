a2e354e28e50707c170dfda20b3e11cd655014a5
carlamariacoppola <cpcr01ch@gmail.com>
2018-03-13 13:33:11 +0100

    Update dataset.py

--- src/python/frigus/readers/dataset.py
+++ src/python/frigus/readers/dataset.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#    dataset.py is part of Frigus.
+
+#    Frigus: software to compure the energy exchange in a multi-level system
+#    Copyright (C) 2016-2018 Mher V. Kazandjian and Carla Maria Coppola
+
+#    Frigus is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License.    
+#
+#    Frigus is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Frigus.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 module that provides functions to load various data in a standard format.
 No parsing is done in this module. The parsing is done in other modules
