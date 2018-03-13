1cdf00ab1fed5f914c8b492a185085077dbcd729
Mher Kazandjian <mherkazandjian@gmail.com>
2018-03-13 23:25:14 +0100

    add placeholder  test module for the wrathmall reader

---
+++ src/python/tests/test_read_cr_wrathmall.py
@@ -0,0 +1,9 @@
+from frigus.readers.read_cr_wrathmall import Reader
+
+import pytest
+
+pytestmark = pytest.mark.skipif(True, reason='developemnt script')
+
+
+def test_that_read_can_be_constructed():
+    raise NotImplementedError('not implemented')
