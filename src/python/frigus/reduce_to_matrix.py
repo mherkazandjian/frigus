b52994a49ac4023e7c1bcba831ed52a1936f9ec0
carlamariacoppola <cpcr01ch@gmail.com>
2018-03-13 13:31:19 +0100

    Update reduce_to_matrix.py

--- src/python/frigus/reduce_to_matrix.py
+++ src/python/frigus/reduce_to_matrix.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#    reduce_to_matrix.py is part of Frigus.
+
+#    Frigus: software to compure the energy exchange in a multi-level system
+#    Copyright (C) 2016-2018 Mher V. Kazandjian and Carla Maria Coppola
+
+#    Frigus is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License.    
+#
+#    Frigus is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Frigus.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 """
