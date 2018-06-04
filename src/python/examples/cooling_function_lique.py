1aabfe971da9deb9c7591f4c746dac32ceefef9b
Carla Maria Coppola <cpcr01ch@gmail.com>
2018-06-04 17:17:08 +0200

    prepare script for producing the figure of the cooling function with a proper mesh

--- src/python/examples/cooling_function_lique.py
+++ src/python/examples/cooling_function_lique.py
@@ -13,6 +13,9 @@ from frigus.cooling_function.fits import fit_glover
 from frigus.population import cooling_rate_at_steady_state
 from frigus.readers.dataset import DataLoader
 
+import matplotlib
+from matplotlib.ticker import ScalarFormatter
+
 from decimal import Decimal
 
 species_data = DataLoader().load('H2_lique')
@@ -81,6 +84,24 @@ axs.loglog(
     t_rng.value, lambda_vs_t_kin_glover.to(u.Joule / u.second).value,
     'r--', color='blue'
 )
+
+
+axs.set_yticks([1e-36, 2e-36, 3e-36, 4e-36, 5e-36, 6e-36, 7e-36, 8e-36, 9e-36,
+                1e-35, 2e-35, 3e-35, 4e-35, 5e-35, 6e-35, 7e-35, 8e-35, 9e-35,
+                1e-34, 2e-34, 3e-34, 4e-34, 5e-34, 6e-34, 7e-34, 8e-34, 9e-34,
+                1e-33, 2e-33, 3e-33, 4e-33, 5e-33, 6e-33, 7e-33, 8e-33, 9e-33,
+                1e-32, 2e-32, 3e-32, 4e-32, 5e-32, 6e-32, 7e-32, 8e-32, 9e-32,
+                1e-31, 2e-31, 3e-31, 4e-31, 5e-31, 6e-31, 7e-31, 8e-31, 9e-31,
+                1e-30, 2e-30, 3e-30, 4e-30, 5e-30, 6e-30, 7e-30, 8e-30, 9e-30,
+                1e-29, 2e-29, 3e-29, 4e-29, 5e-29, 6e-29, 7e-29, 8e-29, 9e-29,
+                1e-28, 2e-28, 3e-28, 4e-28, 5e-28, 6e-28, 7e-28, 8e-28, 9e-28,
+                1e-27, 2e-27, 3e-27, 4e-27, 5e-27, 6e-27, 7e-27, 8e-27, 9e-27,
+                1e-26, 2e-26, 3e-26])
+
+
+matplotlib.pyplot.ylim(ymin = 3e-36, ymax=3e-26)
+
+axs.get_yaxis().set_minor_formatter(matplotlib.ticker.ScalarFormatter())
 
 plt.legend()
 plt.show()
