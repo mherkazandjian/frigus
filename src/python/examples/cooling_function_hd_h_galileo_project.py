remove the plt_ion to show the figure in non-interactive mode + fix the units in the caption of the figures

src/python/examples/cooling_function_hd_h_galileo_project.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Calculate equilibrium population of species and the cooling function using
-the Lipovka data
+the data by Benjamin Desrorreaux et al 2018
 """
 from __future__ import print_function
 import numpy as np
@@ -16,23 +16,27 @@ from frigus.readers.dataset import DataLoader
 species_data = DataLoader().load('HD_galileo_project')
 species_data_old = DataLoader().load('HD_lipovka')
 
-plt.ion()
-fig, axs = plt.subplots(figsize=(8, 8))
+
 
 
 # density of the colliding species, in m^3
+#nc_h_rng = [
+#               1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14
+#           ] * u.meter ** -3
 nc_h_rng = [
-               1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14
+               1.e6
            ] * u.meter ** -3
 t_rad = 0.0 * u.Kelvin
 
-t_rng = np.logspace(2, 3.1, 10) * u.Kelvin
+t_rng = np.logspace(2.5, 3.1, 25) * u.Kelvin
 #T_rng = species_data.raw_data.collision_rates_T_range
 #T_rng_old = species_data_old.raw_data.collision_rates_T_range
 
 
 
 plot_markers = [(2 + i//2, 1 + i % 2, 0) for i in range(16)]
+
+fig, axs = plt.subplots(figsize=(10, 10))
 
 for nc_index, nc_h in enumerate(nc_h_rng):
 
@@ -65,12 +69,15 @@ for nc_index, nc_h in enumerate(nc_h_rng):
 
     lambda_vs_t_kin_lipovka = fit_lipovka(t_rng, nc_h)
 
-    axs.loglog(
-        t_rng.value, lambda_vs_t_kin.cgs.value,
+    axs.set_yscale('linear')
+    axs.set_xscale('linear')
+
+    axs.plot(
+        t_rng.value, lambda_vs_t_kin.value,
         '-x', color='black', marker=plot_markers[nc_index], label=''
     )
-    axs.loglog(
-        t_rng.value, lambda_vs_t_kin_old.cgs.value,
+    axs.plot(
+        t_rng.value, lambda_vs_t_kin_old.value,
         '-x', color='red', marker=plot_markers[nc_index], label=''
     )
 
@@ -81,7 +88,7 @@ for nc_index, nc_h in enumerate(nc_h_rng):
 #    )
 
     axs.set_xlabel('T$_\mathrm{kin}$ [K]')
-    axs.set_ylabel('cooling function [erg s$^{-1}$]')
+    axs.set_ylabel('cooling function [eV s$^{-1}$]')
 
 plt.legend()
 plt.show()