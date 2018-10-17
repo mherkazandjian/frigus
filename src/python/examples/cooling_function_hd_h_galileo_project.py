fef7142e8f066515aa29a6ccdd98fb52bd18e4d8
Carla Maria Coppola <cpcr01ch@gmail.com>
2018-10-17 15:38:45 +0200

    compare updated calculations with the results by flower and by lipovka

--- src/python/examples/cooling_function_hd_h_galileo_project.py
+++ src/python/examples/cooling_function_hd_h_galileo_project.py
@@ -14,21 +14,17 @@ from frigus.population import cooling_rate_at_steady_state
 from frigus.readers.dataset import DataLoader
 
 species_data = DataLoader().load('HD_galileo_project')
-species_data_old = DataLoader().load('HD_lipovka')
-
-
-
 
 # density of the colliding species, in m^3
 #nc_h_rng = [
 #               1.e6, 1.e7, 1.e8, 1.e9, 1.e10, 1.e11, 1.e12, 1.e13, 1.e14
 #           ] * u.meter ** -3
 nc_h_rng = [
-               1.e24
+               1.e6
            ] * u.meter ** -3
 t_rad = 0.0 * u.Kelvin
 
-t_rng = np.logspace(2.5, 3.1, 25) * u.Kelvin
+t_rng = np.logspace(1, 3.1, 30) * u.Kelvin
 #T_rng = species_data.raw_data.collision_rates_T_range
 #T_rng_old = species_data_old.raw_data.collision_rates_T_range
 
@@ -41,19 +37,11 @@ fig, axs = plt.subplots(figsize=(10, 10))
 for nc_index, nc_h in enumerate(nc_h_rng):
 
     lambda_vs_t_kin = []
-    lambda_vs_t_kin_old = []
     pop_dens_vs_t_kin = []
 
     for t_kin in t_rng:
         print(t_kin, nc_h)
 
-        lambda_vs_t_kin_old += [
-            cooling_rate_at_steady_state(
-                species_data_old,
-                t_kin,
-                t_rad,
-                nc_h)
-        ]
 
         lambda_vs_t_kin += [
             cooling_rate_at_steady_state(
@@ -64,29 +52,31 @@ for nc_index, nc_h in enumerate(nc_h_rng):
         ]
 
 
-    lambda_vs_t_kin_old = u.Quantity(lambda_vs_t_kin_old)
     lambda_vs_t_kin = u.Quantity(lambda_vs_t_kin)
 
-    lambda_vs_t_kin_lipovka = fit_lipovka(t_rng, nc_h)
+    lambda_vs_t_kin = lambda_vs_t_kin.to(u.erg / u.second)
 
-    lambda_vs_t_kin_flower = fit_flower(nc_h, t_rng)
 
-    lambda_vs_t_kin_flower = lambda_vs_t_kin_flower.to(u.eV / u.second)
+    lambda_vs_t_kin_lipovka = fit_lipovka(t_rng[t_rng.value >=100], nc_h)
+
+    lambda_vs_t_kin_flower = fit_flower(nc_h, t_rng[t_rng.value >=30])
+
+    #lambda_vs_t_kin_flower = lambda_vs_t_kin_flower.to(u.eV / u.second)
 
     axs.set_yscale('linear')
     axs.set_xscale('linear')
 
-    axs.plot(
+    axs.loglog(
         t_rng.value, lambda_vs_t_kin.value,
-        'o', color='black', label=''
+        'o', color='black', label='present calculation'
     )
-    axs.plot(
-        t_rng.value, lambda_vs_t_kin_old.value,
-        '-.x', color='red', marker=plot_markers[nc_index], label=''
+    axs.loglog(
+        t_rng[t_rng.value >= 100.], lambda_vs_t_kin_lipovka.value,
+        '-.x', color='red', marker=plot_markers[nc_index], label='fit by Lipovka et al. 2005'
     )
-    axs.plot(
-        t_rng.value, lambda_vs_t_kin_flower.value,
-        'r--', color='green', marker=plot_markers[nc_index], label=''
+    axs.loglog(
+        t_rng[t_rng.value >= 30.], lambda_vs_t_kin_flower.value,
+        'r--', color='green', marker=plot_markers[nc_index], label='fit on data by Flower et al. 2000'
     )
 
 
@@ -97,7 +87,7 @@ for nc_index, nc_h in enumerate(nc_h_rng):
 #    )
 
     axs.set_xlabel('T$_\mathrm{kin}$ [K]')
-    axs.set_ylabel('cooling function [eV s$^{-1}$]')
+    axs.set_ylabel('cooling function [erg s$^{-1}$]')
 
 plt.legend()
 plt.show()
